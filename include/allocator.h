ecd4e07219178f75a14038f89b94b67738a6b442
FurryBasket9978 <pgillbama@icloud.com>
2025-09-02 19:19:22 -0500

    added files

---
+++ include/allocator.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <sys/mman.h>
+
+void* reserve(size_t size);
---
+++ src/allocator.c
@@ -0,0 +1,12 @@
+#include "allocator.h"
+
+void* reserver(size_t size){
+    char file_name[] = "allocation.txt";
+    size_t file_size = 4096;
+    int fd = open()
+    void* ptr = mmap(NULL, )
+}
+
+int main(){
+    return 0;
+}
