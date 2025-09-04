61e763ce29ebe169a3c4ba263e7da7222207022c
PhilipGill402 <pgillbama@icloud.com>
2025-09-03 19:11:37 -0500

    made a basic memory allocator, it allocates a whole page at a time though which isn't efficient

--- include/allocator.h
+++ include/allocator.h
@@ -1,4 +1,22 @@
 #pragma once
+#include <fcntl.h>
 #include <sys/mman.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/resource.h>
+
+#define BLOCK_SIZE sizeof(Block)
+#define PAGE_SIZE sysconf(_SC_PAGESIZE)
+
+typedef uintptr_t word_t;
+
+typedef struct Block {
+    size_t size;
+} Block;
 
 void* reserve(size_t size);
+void release(void* ptr);
--- src/allocator.c
+++ src/allocator.c
@@ -1,12 +1,48 @@
 #include "allocator.h"
 
-void* reserver(size_t size){
-    char file_name[] = "allocation.txt";
-    size_t file_size = 4096;
-    int fd = open()
-    void* ptr = mmap(NULL, )
+size_t align(size_t size, size_t alignment){
+    return (size + (alignment - 1)) & ~(alignment - 1);
+}
+
+void* reserve(size_t size){
+    struct Block* block;
+    size += BLOCK_SIZE;
+
+    size_t aligned_size = align(size + BLOCK_SIZE, PAGE_SIZE);
+
+    block = mmap(NULL, aligned_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+   
+
+    if (block == MAP_FAILED){
+        perror("mmap"); 
+        return NULL;
+    }
+    
+    block->size = aligned_size;
+
+
+    return (void*)(block + 1);
+}
+
+void release(void* ptr){
+    if (ptr == NULL){
+        return;
+    }
+
+    struct Block* block = ((struct Block*)ptr) - 1;
+    int ret = munmap(block, block->size);
+    
+    if (ret == -1){
+        perror("munmap"); 
+        abort();
+    } 
 }
 
 int main(){
+    size_t size = sizeof(int);
+    int* p = reserve(size);
+
+
+    release(p); 
     return 0;
 }
